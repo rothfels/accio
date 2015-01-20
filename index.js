b2c63ce57f8fc587582bfed8062f221a57b35727
John Rothfels <john.rothfels@gmail.com>
2015-01-19 19:12:46 -0800

    more tweaks

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var multer  = require('multer')
 
 var app = express();
 var mailBody = {};
@@ -7,6 +8,9 @@ var mailBody = {};
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
+
 
 app.get('/', function(request, response) {
   response.json(mailBody);
