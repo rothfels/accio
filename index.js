5921bf98beebeba678da348817f6b8046d32ce9e
John Rothfels <john.rothfels@gmail.com>
2015-01-19 21:13:55 -0800

    collect senders and send on trigger email address

--- index.js
+++ index.js
@@ -1,27 +1,64 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var multer  = require('multer')
+var multer = require('multer');
+var _ = require('lodash');
+
+// Email adress which sends to all addresses in the ad hoc collection.
+var triggerAddress = 'john.rothfels@gmail.com'
+
+// Set your mailgun API key here.
+var api_key = 'key-35d22fa46e85f8090bc8d205648d5588';
+// var domain = 'johnrothfels.com.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: 'johnrothfels.com'});
 
 var app = express();
-var mailBody = {};
+var adHocCollection = {};
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
+
+app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(multer()); // for parsing multipart/form-data
 
-
-app.get('/', function(request, response) {
-  response.json(mailBody);
+app.get('/', function(req, res) {
+  res.json(adHocCollection);
 });
 
-app.post('/mail', function(request, response) {
-  // console.log(request.param('sender'));
-  // console.log(request.params);
-  console.log(request.body);
-  mailBody = request.body;
-  response.sendStatus(200);
+app.get('/clear', function(req, res) {
+  adHocCollection = {};
+  res.send('All clear!');
+})
+
+var sendMessages = function(subject, text) {
+  _.forEach(_.keys(adHocCollection), function(recipient) {
+    var data = {
+      from: 'John Rothfels <me@johnrothfels.com>',
+      to: recipient,
+      subject: subject,
+      text: text
+    };
+
+    mailgun.messages().send(data, function(err, body) {
+      if (err) {
+        console.log("ERROR");
+        console.log(err);
+      } else {
+        console.log(body);
+      }
+    };
+  });
+};
+
+app.post('/mail', function(req, res) {
+  var sender = req.body.sender;
+  if (sender === triggerAddress) {
+    sendMessages(req.body.subject);
+  } else {
+    adHocCollection[req.body.sender] = true;
+  }
+
+  res.sendStatus(200);
 });
 
 app.listen(app.get('port'), function() {
