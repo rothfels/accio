cd05f6c72f3eb3ff44fc0f8721467590d4226f3a
John Rothfels <john.rothfels@gmail.com>
2015-01-19 18:44:40 -0800

    parse inbound mail

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-var express = require('express')
-var app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var app = express();
+var mailBody = {};
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.json(mailBody);
+});
+
+app.post('/mail', function(request, response) {
+  mailBody = request.body;
+  response.json(mailBody);
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
